03dd7e748c640f552827865f69b3192fd766d717
Lucas Santos Silva <lucassantos@sms.rootssa.ba.gov.br>
2019-06-26 08:07:31 -0300

    adding categories module

---
+++ src/app/pages/categories/categories-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+
+const routes: Routes = [
+  { path: '', component: CategoryListComponent },
+  { path: ':id', component: CategoryFormComponent },
+  { path: ':id/edit', component: CategoryFormComponent }
+];
+
+//rotas
+//nomesite.com/categories => list (master)
+//nomesite.com/categories/new => form (master)
+//nomesite.com/categories/:id/edit => form (master)
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CategoriesRoutingModule { }
+
+
+

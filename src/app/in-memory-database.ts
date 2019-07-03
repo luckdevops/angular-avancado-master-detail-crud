2c1fc2957e1571c08a91bb45d4fbea3702db5f8e
Lucas Santos Silva <lucassantos@sms.rootssa.ba.gov.br>
2019-07-03 17:39:03 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,12 +1,14 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
+import { Category } from "./pages/categories/shared/category.model";
+
 export class InMemoryDatabase implements InMemoryDbService {
 
     createDb() {
       const categories = [
-        { id: 1, nome: 'Lazer', description: 'Cinema, parques, praia, etc'  }
-        { id: 2, nome: 'Saúde', description: 'Plano de Saúde e Remédios'  }
-        { id: 3, nome: 'Salário', description: 'Recebimebto de Salário'  }
+        { id: 1, nome: 'Lazer', description: 'Cinema, parques, praia, etc'  },
+        { id: 2, nome: 'Saúde', description: 'Plano de Saúde e Remédios'  },
+        { id: 3, nome: 'Salário', description: 'Recebimebto de Salário'  },
         { id: 4, nome: 'Freelas', description: 'Trabalhos como freelancer'  }
       ];
 

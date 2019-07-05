d458f471167b1baf26e04acdeef603825f7bcbd9
Lucas Santos Silva <lucassantos@sms.rootssa.ba.gov.br>
2019-07-05 08:20:14 -0300

    adding base methods to CategoryForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDatabase } from "./in-memory-database";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
 
 @NgModule({
   declarations: [
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Category } from "../shared/category.model";
-import { CategoryService } from "../shared/category.service";
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -22,7 +22,7 @@ export class CategoryListComponent implements OnInit {
   }
 
   deleteCategory(category) {
-    const mustDelete = confirm("Deseja realmente excluir este item?")
+    const mustDelete = confirm('Deseja realmente excluir este item?')
 
     if (mustDelete) {
       this.categoryService.delete(category.id).subscribe(

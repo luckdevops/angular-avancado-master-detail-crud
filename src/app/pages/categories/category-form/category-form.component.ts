510b9935b93f944dc3c11ecda3aa35f53b62d672
Lucas Santos Silva <lucassantos@sms.rootssa.ba.gov.br>
2019-07-09 13:33:25 -0300

    improving create and update of categories

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -41,6 +41,16 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle();
   }
 
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction == 'new') {
+        this.createCategory();
+    } else {
+        this.updateCategory();
+    }
+  }
+
   //PRIVATE METHODS
   private setCurrentAction() {
     if (this.route.snapshot.url[0].path == 'new') {
@@ -80,6 +90,47 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     } else {
         const categoryName = this.category.name || '';
         this.pageTitle = 'Editando Categoria: ' + categoryName;
+    }
+  }
+
+  private createCategory() {
+     const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+     this.categoryService.create(category)
+       .subscribe(
+          category => this.actionsForSucess(category),
+          error => this.actionsForError(error)
+       );
+  }
+
+  private updateCategory() {
+     const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+     this.categoryService.update(category)
+      .subscribe(
+         category => this.actionsForSucess(category),
+         error => this.actionsForError(error)
+      );
+  }
+
+  private actionsForSucess(category: Category) {
+     toastr.success('Solicitacao processada com sucesso!');
+
+     //redirect/reload component page
+     this.router.navigateByUrl('categories', {skipLocationChange: true}).then(
+       () => this.router.navigate(['categories', category.id, 'edit'])
+     );
+  }
+
+  private actionsForError(error) {
+    toastr.error('Ocorreu um erro ao processar sua solicitação!');
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+        this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+        this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tente mais tarde.']
     }
   }
 }
